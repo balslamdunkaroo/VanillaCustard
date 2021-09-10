7c68366833c9bb0c85c963197c454aa0ba25880c
Hindi, Balsam <balsam.hindi@cbsinteractive.com>
2021-09-10 01:40:24 -0400

    Ovelay and marker updates

--- src/VideoApp.js
+++ src/VideoApp.js
@@ -23,21 +23,21 @@ const dummmyModelJSON= [{
     _id: '3',
     nsfw_type: 'violence',
     startTime: 400,
-    endtime: 82
+    endtime: 403
 
 },
   {
     _id: '4',
     nsfw_type: 'nudity',
     startTime: 138,
-    endtime: 82
+    endtime: 200
 
 }];
 
 const nsfwMapper =
     {
-        "nudity": "#964b00",
-        "violence": "#ff0200"
+        "nudity": "#dcd0ff",
+        "violence": "#006400"
     };
 
 function createMarkers(modelJSON){
@@ -58,6 +58,28 @@ function createMarkers(modelJSON){
   return markerArr
 }
 
+function getAllTimestamps(modelJSON){
+    let tagObj = {};
+    let nsfwArr = [];
+    let violenceArr = [];
+    let inapArr = [];
+    for (let obj in modelJSON) {
+        let lowEnd = modelJSON[obj]["startTime"] - 5;
+        let highEnd = modelJSON[obj]["endtime"] - 5;
+        for (let i = lowEnd; i <= highEnd; i++) {
+            if(modelJSON[obj]["nsfw_type"] === 'nudity')
+                nsfwArr.push(i);
+            else if(modelJSON[obj]["nsfw_type"] === 'violence')
+                violenceArr.push(i);
+            else if(modelJSON[obj]["nsfw_type"] === 'sexual_content')
+               inapArr.push(i);}}
+        tagObj["nudity"] = nsfwArr;
+        tagObj['violence'] = violenceArr;
+        tagObj['sexual_content'] = inapArr;
+        console.log("tagobj", tagObj);
+        return tagObj;
+}
+
 function VideoApp (){
   const [url] = useState('https://storage.cloud.google.com/vanilla-custard-bucket/nsfw_collection.mp4');
   const [controls, setControls] = useState([
@@ -67,9 +89,13 @@ function VideoApp (){
     'volume',
     'full-screen'
   ]);
+  const [showOverlay, setOverlay] = useState(false);
+  const [nudeOverlay, setNude] = useState(false);
+  const [violenceOverlay, setViolence] =useState(false);
+  const [inappOverlay, setInapp] = useState(false);
   const [isPlaying, setIsPlaying] = useState(false);
   const [volume, setVolume] = useState(0.7);
-  const [timeStart] = useState(5);
+  const [timeStart] = useState(0);
 
   const controlsList = [
     {
@@ -119,7 +145,24 @@ function VideoApp (){
 
   const handleProgress = e => {
     console.log('Current time: ', e.target.currentTime);
-  };
+    let currTime = Math.round(e.target.currentTime);
+    console.log('Whole Number:', currTime);
+    let timestamps = getAllTimestamps(dummmyModelJSON);
+    let nudeArray = timestamps["nudity"];
+    let violenceArray = timestamps["violence"];
+    if(nudeArray.includes(currTime)){
+        console.log("nude");
+        setNude(true);
+        setViolence(false);}
+    else if(violenceArray.includes(currTime)){
+        console.log("violence");
+        setViolence(true);
+        setNude(false);}
+    else{
+        setOverlay(false);
+        console.log('fuzili')
+        }
+    };
 
   const handleDuration = duration => {
     console.log('Duration: ', duration);
@@ -133,7 +176,7 @@ function VideoApp (){
 
   return (
     <div className="container" style={{ paddingTop: '100px'}}>
-      <VideoPlayer
+        <VideoPlayer id="v"
         url={url}
         controls={controls}
         isPlaying={isPlaying}
@@ -148,9 +191,17 @@ function VideoApp (){
         onVolume={handleVolume}
         onProgress={handleProgress}
         onDuration={handleDuration}
-        onMarkerClick={handleMarkerClick}
-      />
-      <div className="controls">
+        onMarkerClick={handleMarkerClick}>
+        </VideoPlayer>
+        {nudeOverlay === true &&
+        <div className="fade-in">
+            <div id="overlay" style={{"backgroundColor": "rgba(220, 208, 255, 0.44)"}}></div>
+        </div> }
+        {violenceOverlay === true &&
+        <div className="fade-in">
+            <div id="overlay" style={{"backgroundColor": "rgba(255, 2, 0, 0.57)"}}></div>
+        </div> }
+        <div className="controls">
         <p>
           Controls:
           <button onClick={isPlaying ? handlePause : handlePlay}>
@@ -182,11 +233,13 @@ function VideoApp (){
           {controls.length ? '"]' : ''}
         </p>
         <p>isPlaying: {isPlaying.toString()}</p>
+        <p>showOverlay: {showOverlay.toString()}</p>
         <p>volume: {volume}</p>
         <p>timeStart: {timeStart}</p>
       </div>
     </div>
   );
 }
+
 
 export default withRouter(VideoApp);
